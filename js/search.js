d470a2727a72b4534705d755ca32e67233ea5905
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-16 08:01:57 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,12 +6,15 @@
   var searchResults = document.getElementById('search-results');
   var postList = document.getElementById('post-list');
 
-  var pages = [
+  var posts = [
     {% for post in site.posts %}
       {
         title: "{{ post.title | xml_escape }}",
         url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
-        content: "{{ post.content | strip_html | strip_newlines | escape }}"
+        excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+        content: "{{ post.content | strip_html | strip_newlines | escape }}",
+        tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+        categories: "{% for category in post.categories %}{{ category }}{% unless forloop.last %}, {% endunless %}{% endfor %}"
       }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
@@ -19,14 +22,33 @@
   function search(query) {
     var results = [];
 
-    for (var i = 0; i < pages.length; i++) {
-      var page = pages[i];
-      if (page.title.toLowerCase().includes(query.toLowerCase()) || page.content.toLowerCase().includes(query.toLowerCase())) {
-        results.push(page);
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+
+      if (
+        post.title.toLowerCase().includes(query.toLowerCase()) ||
+        post.tags.toLowerCase().includes(query.toLowerCase()) ||
+        post.categories.toLowerCase().includes(query.toLowerCase()) ||
+        post.content.toLowerCase().includes(query.toLowerCase())
+      ) {
+        var highlightedTitle = highlightMatch(post.title, query);
+        var highlightedExcerpt = highlightMatch(post.excerpt, query);
+        results.push({
+          title: highlightedTitle,
+          url: post.url,
+          excerpt: highlightedExcerpt
+        });
       }
     }
 
     return results;
+  }
+
+  function highlightMatch(text, query) {
+    var regex = new RegExp(query, 'gi');
+    return text.replace(regex, function (match) {
+      return '<span class="highlight">' + match + '</span>';
+    });
   }
 
   function renderResults(results) {
@@ -40,8 +62,9 @@
         var li = document.createElement('li');
         var a = document.createElement('a');
         a.href = result.url;
-        a.innerText = result.title;
-        li.appendChild(a);
+        a.innerHTML = result.title;
+        li.innerHTML = result.excerpt;
+        li.insertBefore(a, li.firstChild);
         searchResults.appendChild(li);
       }
     }
