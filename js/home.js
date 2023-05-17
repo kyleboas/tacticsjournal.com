9ebc95b5dd6d5a7e9719a12041d11bb8e13706ee
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-16 22:29:35 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -14,37 +14,30 @@
   ];
 
   function search(query) {
-    var results = [];
+  var results = [];
 
-    if (!query || query.trim() === '') {
-      if (selectedFilter === null || !document.querySelector('.topic.selected')) {
-        return posts; // Return all posts if no query, no filter, and no search query or if no filter is selected
-      }
-      // Apply filter if a search query is present
-      query = '';
+  if (!query || query.trim() === '') {
+    if (selectedFilter === null) {
+      return posts; // Return all posts if no query and no filter selected
     }
-
-    for (var i = 0; i < posts.length; i++) {
-      var post = posts[i];
-
-      if (
+    // Filter posts based on the selected filter
+    results = posts.filter(function (post) {
+      return post.tags.includes(selectedFilter);
+    });
+  } else {
+    // Apply both filter and search query
+    results = posts.filter(function (post) {
+      return (
         (selectedFilter === null || post.tags.includes(selectedFilter)) && // Check if the post matches the selected filter (if any)
         (post.title.toLowerCase().includes(query.toLowerCase()) ||
           post.excerpt.toLowerCase().includes(query.toLowerCase()))
-      ) {
-        var highlightedTitle = highlightMatch(post.title, query);
-        var highlightedExcerpt = highlightMatch(post.excerpt, query);
-        results.push({
-          title: highlightedTitle,
-          url: post.url,
-          excerpt: highlightedExcerpt,
-          tags: post.tags,
-        });
-      }
-    }
-
-    return results;
+      );
+    });
   }
+
+  return results;
+}
+
 
   function highlightMatch(text, query) {
     var regex = new RegExp(query, 'gi');
@@ -54,45 +47,43 @@
   }
 
   function renderResults(results) {
-    postList.innerHTML = '';
+  postList.innerHTML = '';
 
-    if (results.length === 0 && searchInput.value !== '') {
-      searchResults.innerHTML = '<p>No results found.</p>';
-    } else {
-      searchResults.innerHTML = ''; // Clear the search results
-      var postsToRender = searchInput.value === '' ? posts : results;
+  if (results.length === 0 && searchInput.value !== '') {
+    searchResults.innerHTML = '<p>No results found.</p>';
+  } else {
+    searchResults.innerHTML = ''; // Clear the search results
+    var postsToRender = searchInput.value === '' ? posts : results;
 
-      for (var i = 0; i < postsToRender.length; i++) {
-        var result = postsToRender[i];
+    for (var i = 0; i < postsToRender.length; i++) {
+      var result = postsToRender[i];
 
-        // Check if the post's tags include the selected filter (if any)
-        if (selectedFilter === null || result.tags.includes(selectedFilter)) {
-          var li = document.createElement('li');
-          li.classList.add('post-item'); // Add a custom class for styling purposes
-          var a = document.createElement('a');
-          a.href = result.url;
-          a.innerHTML = result.title;
-          li.appendChild(a);
-          var p = document.createElement('p');
-          p.innerHTML = result.excerpt;
-          li.appendChild(p);
+      var li = document.createElement('li');
+      li.classList.add('post-item'); // Add a custom class for styling purposes
+      var a = document.createElement('a');
+      a.href = result.url;
+      a.innerHTML = result.title;
+      li.appendChild(a);
+      var p = document.createElement('p');
+      p.innerHTML = result.excerpt;
+      li.appendChild(p);
 
-          // Add tags for the post
-          var tags = document.createElement('span');
-          tags.classList.add('tags');
-          tags.style.display = 'none'; // Hide the tags initially
-          tags.innerHTML = result.tags.join(', '); // Assuming the tags are an array of strings
-          li.appendChild(tags);
+      // Add tags for the post
+      var tags = document.createElement('span');
+      tags.classList.add('tags');
+      tags.style.display = 'none'; // Hide the tags initially
+      tags.innerHTML = result.tags.join(', '); // Assuming the tags are an array of strings
+      li.appendChild(tags);
 
-          postList.appendChild(li);
-        }
-      }
-    }
-
-    if (selectedFilter === null) {
-      selectedFilter = document.querySelector('.topic.selected') ? document.querySelector('.topic.selected').getAttribute('data-name') : null;
+      postList.appendChild(li);
     }
   }
+
+  if (selectedFilter === null) {
+    selectedFilter = document.querySelector('.topic.selected') ? document.querySelector('.topic.selected').getAttribute('data-name') : null;
+  }
+}
+
 
   var filterLinks = document.querySelectorAll('.topic');
 
