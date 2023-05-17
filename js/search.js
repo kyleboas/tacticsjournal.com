016eca48ed32da8dc8d3997baebded90012b8398
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-17 07:49:37 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -86,6 +86,54 @@
     return results;
   }
 
+(function () {
+  var searchInput = document.getElementById('search-input');
+  var searchResults = document.getElementById('search-results');
+  var postList = document.getElementById('post-list');
+
+  var posts = [
+    {% for post in site.posts %}
+      {
+        title: "{{ post.title | xml_escape }}",
+        url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+        excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+        tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+        category: "{{ post.category }}"
+      }{% unless forloop.last %},{% endunless %}
+    {% endfor %}
+  ];
+
+  function search(query) {
+    var results = [];
+
+    if (!query || query.trim() === '') {
+      return posts; // Return all posts if no query is provided or if it's blank
+    }
+
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+
+      if (
+        post.title.toLowerCase().includes(query.toLowerCase()) ||
+        post.excerpt.toLowerCase().includes(query.toLowerCase()) ||
+        post.tags.toLowerCase().includes(query.toLowerCase()) ||
+        post.category.toLowerCase().includes(query.toLowerCase())
+      ) {
+        var highlightedTitle = highlightMatch(post.title, query);
+        var highlightedExcerpt = highlightMatch(post.excerpt, query);
+        results.push({
+          title: highlightedTitle,
+          url: post.url,
+          excerpt: highlightedExcerpt,
+          tags: post.tags,
+          category: post.category
+        });
+      }
+    }
+
+    return results;
+  }
+
   function highlightMatch(text, query) {
     var regex = new RegExp(query, 'gi');
     return text.replace(regex, function (match) {
