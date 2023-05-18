baf18b0da1ce9b330c5ae1e535c9129f2a464aa3
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-18 17:00:58 -0400

    Create TagsCategories.js

---
+++ _includes/TagsCategories.js
@@ -0,0 +1,23 @@
+{% assign allTags = "" %}
+{% assign allCategories = "" %}
+
+{% for post in site.posts %}
+  {% assign postTags = post.tags | join: "," %}
+  {% assign postCategories = post.categories | join: "," %}
+
+  {% assign allTags = allTags | append: postTags | append: "," %}
+  {% assign allCategories = allCategories | append: postCategories | append: "," %}
+{% endfor %}
+
+let TagsCategories = [];
+
+// Add tags to TagsCategories array
+let tags = "{{ allTags | split: ',' | uniq | join: '","' }}";
+TagsCategories.push(...tags.split(","));
+
+// Add categories to TagsCategories array
+let categories = "{{ allCategories | split: ',' | uniq | join: '","' }}";
+TagsCategories.push(...categories.split(","));
+
+// Remove duplicates from the TagsCategories array
+TagsCategories = [...new Set(TagsCategories)];
