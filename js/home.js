c363219abd08c4566f206c858dc4158cd7720741
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-18 12:42:05 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 ---
 ---
-  
+
   
 (function () {
   var searchInput = document.getElementById('search-input');
@@ -35,56 +35,56 @@
         post.tags.toLowerCase().includes(query.toLowerCase()) ||
         post.categories.toLowerCase().includes(query.toLowerCase()) // Include search in categories
       ) {
-        results.push(post);
+        var highlightedTitle = highlightMatch(post.title, query);
+        var highlightedExcerpt = highlightMatch(post.excerpt, query);
+        results.push({
+          title: highlightedTitle,
+          url: post.url,
+          excerpt: highlightedExcerpt,
+          tags: post.tags
+        });
       }
     }
 
-    return results.slice(0, 5); // Return top 5 matching posts
+    return results;
   }
 
-  function renderSuggestions(suggestions) {
-    searchResults.innerHTML = '';
-
-    for (var i = 0; i < suggestions.length; i++) {
-      var suggestion = suggestions[i];
-      var li = document.createElement('li');
-      li.textContent = suggestion.tags;
-      li.addEventListener('click', function () {
-        searchInput.value = this.textContent; // Autocomplete the search field with the clicked suggestion
-        searchInput.focus(); // Set focus back to the search input
-      });
-      searchResults.appendChild(li);
-    }
-  }
-
-  function renderPosts(posts) {
+  function renderResults(results) {
     postList.innerHTML = '';
+    searchResults.innerHTML = ''; // Clear any previous messages
 
-    for (var i = 0; i < posts.length; i++) {
-      var post = posts[i];
-      var li = document.createElement('li');
-      li.classList.add('post-item'); // Add a custom class for styling purposes
-      var a = document.createElement('a');
-      a.href = post.url;
-      a.innerHTML = post.title;
-      li.appendChild(a);
-      var p = document.createElement('p');
-      p.innerHTML = post.excerpt;
-      li.appendChild(p);
-      postList.appendChild(li);
+    if (results.length === 0 && searchInput.value.trim() !== '') {
+      searchResults.innerHTML = '<p>No results found.</p>'; // Show message only when there are no results and the search input is not empty
+    } else {
+      var postsToRender = searchInput.value.trim() === '' ? posts : results;
+
+      for (var i = 0; i < postsToRender.length; i++) {
+        var result = postsToRender[i];
+        var li = document.createElement('li');
+        li.classList.add('post-item'); // Add a custom class for styling purposes
+        var a = document.createElement('a');
+        a.href = result.url;
+        a.innerHTML = result.title;
+        li.appendChild(a);
+        var p = document.createElement('p');
+        p.innerHTML = result.excerpt;
+        li.appendChild(p);
+        postList.appendChild(li);
+      }
     }
   }
 
-  function handleSearchInput() {
-    var query = searchInput.value.trim();
-    var results = search(query);
-
-    renderSuggestions(results);
-    renderPosts(results);
+  function highlightMatch(text, query) {
+    // Add your highlighting logic here
+    return text;
   }
 
-  searchInput.addEventListener('input', handleSearchInput);
+  searchInput.addEventListener('input', function () {
+    var query = searchInput.value;
+    var results = search(query);
+    renderResults(results);
+  });
 
   // Initial render of all posts
-  renderPosts(posts);
+  renderResults(posts);
 })();
