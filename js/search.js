82569519a8921516ef5c9dd24500c8be1a153821
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-18 20:20:28 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,45 +1,54 @@
 ---
 ---
 
- 
+  
 (function () {
   var searchInput = document.getElementById('search-input');
   var suggestionList = document.getElementById('suggestion-list');
   var postList = document.getElementById('post-list');
 
   var posts = [
-    // Posts data
-  ];
+  {% for post in site.posts %}
+    {
+      title: "{{ post.title | xml_escape }}",
+      url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+      excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+      tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+      category: "{{ post.category | xml_escape }}"
+    }{% unless forloop.last %},{% endunless %}
+  {% endfor %}
+   ];
+
 
   function search(query) {
-    var results = [];
+  var results = [];
 
-    if (!query || query.trim() === '') {
-      return posts; // Return all posts if no query is provided or if it's blank
-    }
-
-    for (var i = 0; i < posts.length; i++) {
-      var post = posts[i];
-
-      if (
-        post.title.toLowerCase().includes(query.toLowerCase()) ||
-        post.excerpt.toLowerCase().includes(query.toLowerCase()) ||
-        post.tags.toLowerCase().includes(query.toLowerCase()) || // Add search in tags
-        post.category.toLowerCase().includes(query.toLowerCase()) // Add search in category
-      ) {
-        var highlightedTitle = highlightMatch(post.title, query);
-        var highlightedExcerpt = highlightMatch(post.excerpt, query);
-        results.push({
-          title: highlightedTitle,
-          url: post.url,
-          excerpt: highlightedExcerpt,
-          tags: post.tags
-        });
-      }
-    }
-
-    return results;
+  if (!query || query.trim() === '') {
+    return posts; // Return all posts if no query is provided or if it's blank
   }
+
+  for (var i = 0; i < posts.length; i++) {
+    var post = posts[i];
+
+    if (
+      post.title.toLowerCase().includes(query.toLowerCase()) ||
+      post.excerpt.toLowerCase().includes(query.toLowerCase()) ||
+      post.tags.toLowerCase().includes(query.toLowerCase()) || // Add search in tags
+      post.category.toLowerCase().includes(query.toLowerCase()) // Add search in category
+    ) {
+      var highlightedTitle = highlightMatch(post.title, query);
+      var highlightedExcerpt = highlightMatch(post.excerpt, query);
+      results.push({
+        title: highlightedTitle,
+        url: post.url,
+        excerpt: highlightedExcerpt,
+        tags: post.tags
+      });
+    }
+  }
+
+  return results;
+}
 
   function highlightMatch(text, query) {
     var regex = new RegExp(query, 'gi');
@@ -52,28 +61,28 @@
     postList.innerHTML = '';
     suggestionList.innerHTML = ''; // Clear any previous messages
 
-    if (results.length === 0 && searchInput.value.trim() !== '') {
-      postList.innerHTML = '<p>No posts found.</p>'; // Show message only when there are no results and the search input is not empty
-    } else {
-      var postsToRender = searchInput.value.trim() === '' ? posts : results;
+  if (results.length === 0 && searchInput.value.trim() !== '') {
+    searchResults.innerHTML = '<p>No results found.</p>'; // Show message only when there are no results and the search input is not empty
+  } else {
+    var postsToRender = searchInput.value.trim() === '' ? posts : results;
 
-      for (var i = 0; i < postsToRender.length; i++) {
-        var result = postsToRender[i];
-        var li = document.createElement('li');
-        li.classList.add('post-item'); // Add a custom class for styling purposes
-        var a = document.createElement('a');
-        a.href = result.url;
-        a.innerHTML = result.title;
-        li.appendChild(a);
-        var p = document.createElement('p');
-        p.innerHTML = result.excerpt;
-        li.appendChild(p);
-        postList.appendChild(li);
-      }
+    for (var i = 0; i < postsToRender.length; i++) {
+      var result = postsToRender[i];
+      var li = document.createElement('li');
+      li.classList.add('post-item'); // Add a custom class for styling purposes
+      var a = document.createElement('a');
+      a.href = result.url;
+      a.innerHTML = result.title;
+      li.appendChild(a);
+      var p = document.createElement('p');
+      p.innerHTML = result.excerpt;
+      li.appendChild(p);
+      postList.appendChild(li);
     }
   }
-
-  // Get the search query from the URL
+}
+ 
+ // Get the search query from the URL
   var searchQuery = new URLSearchParams(window.location.search).get('search');
   if (searchQuery) {
     searchInput.value = searchQuery;
