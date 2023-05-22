4c6fd79f1d0f0e84ba5daf8e6cb6fd28bfa396af
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-05-22 12:20:45 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -17,6 +17,7 @@
     var image = document.createElement('img');
     image.src = '{{ site.baseurl }}/images/FE0024A5-5B8C-4CB7-84A7-0A88C8801B63.jpeg';
     image.style.width = '100px';
+    image.style.paddingTop = '10px';
     imageLink.appendChild(image);
     noResultsMessage.appendChild(imageLink);
 
