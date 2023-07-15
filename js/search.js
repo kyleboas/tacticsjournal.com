87f3c7f2288bed7ac39a9814d2273f1f3f9b5c9f
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-07-15 18:54:32 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,16 +25,15 @@
   }
 
   var posts = [
-  {% for post in site.posts %}
-    {
-      title: "{{ post.title | xml_escape }}",
-      url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
-      excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-      content: "{{ post.content | escape }}",
-      tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
-      categories: "{{ post.categories | xml_escape }}",
-      date: "{{ post.date | date: "%B %d, %Y" }}"
-    }{% unless forloop.last %},{% endunless %}
+    {% for post in site.posts %}
+        {
+          title: "{{ post.title | xml_escape }}",
+          url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+          excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+          categories: "{{ post.categories | xml_escape }}",
+          date: "{{ post.date | date: "%B %d, %Y" }}"
+        }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
 
@@ -118,21 +117,21 @@
 
         // Check if the post has the category "Notes"
         if (result.categories.includes('Notes')) {
-         // Display the post without the title
-          var contentContainer = document.createElement('div');
-          contentContainer.classList.add('post-content'); // Add a custom class for styling purposes
-          contentContainer.innerHTML = result.content; // Assuming `result.content` contains the full post content
-          li.appendChild(contentContainer);
+          // Display the post without the title
+          var p = document.createElement('p');
+          p.innerHTML = result.excerpt;
+          li.appendChild(p);
         } else {
-         // Display the post with the title and excerpt
+          // Display the post with the title and excerpt
           var a = document.createElement('a');
           a.href = result.url;
           a.innerHTML = result.title;
           li.appendChild(a);
           var p = document.createElement('p');
           p.innerHTML = result.excerpt;
-        li.appendChild(p);
-       }
+          li.appendChild(p);
+        }
+
         postList.appendChild(li);
       }
     } else if (results.length === 0) {
@@ -173,11 +172,10 @@
 
         // Check if the post has the category "Notes"
         if (result.categories.includes('Notes')) {
-         // Display the post without the title
-          var contentContainer = document.createElement('div');
-          contentContainer.classList.add('post-content'); // Add a custom class for styling purposes
-          contentContainer.innerHTML = result.content; // Assuming `result.content` contains the full post content
-          li.appendChild(contentContainer);
+          // Display the post without the title
+          var p = document.createElement('p');
+          p.innerHTML = result.excerpt;
+          li.appendChild(p);
         } else {
           // Display the post with the title and excerpt
           var a = document.createElement('a');
