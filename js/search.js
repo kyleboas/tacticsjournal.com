e31c4acfad5710867751fcc622c4e4bcdd734e58
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-07-16 14:19:22 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -29,7 +29,7 @@
       {
         title: "{{ post.title | xml_escape }}",
         url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
-        content: "{{ post.content | escape }}",
+        content: "{{ post.content | xml_escape }}",
         tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
         categories: "{{ post.categories | xml_escape }}",
         date: "{{ post.date | date: "%B %d, %Y" }}"
