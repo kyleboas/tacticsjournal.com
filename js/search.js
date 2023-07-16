5c23d8daee3f7b1fc23b6ef8e1c3163a015764e1
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-07-16 16:14:47 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,18 +25,17 @@
   }
 
   var posts = [
-  {% for post in site.posts %}
-    {
-      title: "{{ post.title | xml_escape }}",
-      url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
-      excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-      content: "{{ post.content | strip_newlines }}",
-      tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
-      categories: "{{ post.categories | xml_escape }}",
-      date: "{{ post.date | date: "%B %d, %Y" }}"
-    }{% unless forloop.last %},{% endunless %}
-  {% endfor %}
-];
+    {% for post in site.posts %}
+        {
+          title: "{{ post.title | xml_escape }}",
+          url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+          excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+          categories: "{{ post.categories | xml_escape }}",
+          date: "{{ post.date | date: "%B %d, %Y" }}"
+        }{% unless forloop.last %},{% endunless %}
+    {% endfor %}
+  ];
 
   function search(query) {
     var results = [];
@@ -125,9 +124,7 @@
         a.innerHTML = result.title;
         a.classList.add('title');
         p.appendChild(a);
-        var contentParagraph = document.createElement('p');
-        contentParagraph.textContent = result.content;
-        p.appendChild(contentParagraph);
+        p.innerHTML += result.excerpt;
         li.appendChild(p);
         } else {
           // Display the post with the title and excerpt
@@ -187,9 +184,7 @@
         a.innerHTML = result.title;
         a.classList.add('title');
         p.appendChild(a);
-        var contentParagraph = document.createElement('p');
-        contentParagraph.textContent = result.content;
-        p.appendChild(contentParagraph);
+        p.innerHTML += result.excerpt;
         li.appendChild(p);
         } else {
           // Display the post with the title and excerpt
