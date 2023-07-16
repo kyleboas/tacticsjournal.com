d2666563449bcc4e5324fd273c73723faa7a5c43
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-07-16 14:41:17 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,18 +25,18 @@
   }
 
   var posts = [
-    {% for post in site.posts %}
-        {
-          title: "{{ post.title | xml_escape }}",
-          url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
-          excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-          content: "{{ post.content | xml_escape }}",
-          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
-          categories: "{{ post.categories | xml_escape }}",
-          date: "{{ post.date | date: "%B %d, %Y" }}"
-        }{% unless forloop.last %},{% endunless %}
-    {% endfor %}
-  ];
+  {% for post in site.posts %}
+    {
+      title: "{{ post.title | xml_escape }}",
+      url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+      excerpt: "{{ post.excerpt | strip_newlines | escape }}",
+      content: "{{ post.content | escape }}",
+      tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+      categories: "{{ post.categories | xml_escape }}",
+      date: "{{ post.date | date: "%B %d, %Y" }}"
+    }{% unless forloop.last %},{% endunless %}
+  {% endfor %}
+];
 
   function search(query) {
     var results = [];
