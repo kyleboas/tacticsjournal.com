92105c4bf5cb0a1e3559169a35472ed7541e54e4
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-08-10 19:19:23 -0400

    Update search-test.js

--- js/search-test.js
+++ js/search-test.js
@@ -33,7 +33,7 @@
           tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
           categories: "{{ post.categories | xml_escape }}",
           date: "{{ post.date | date: "%B %d, %Y" }}",
-          content: "{{ post.content | remove_newlines | xml_escape | escape }}"
+          note: "{{ post.content }}"
         }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
@@ -125,7 +125,7 @@
         a.innerHTML = result.title;
         a.classList.add('title');
         p.appendChild(a);
-        p.innerHTML += result.content;
+        p.innerHTML += result.note;
         li.appendChild(p);
         } else {
           // Display the post with the title and excerpt
@@ -185,7 +185,7 @@
         a.innerHTML = result.title;
         a.classList.add('title');
         p.appendChild(a);
-        p.innerHTML += result.content;
+        p.innerHTML += result.note;
         li.appendChild(p);
         } else {
           // Display the post with the title and excerpt
