d574ede63c75b69f2f850195f7f0c56c6d881c08
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-08-20 16:15:37 -0400

    Update search-post.js

--- js/search-post.js
+++ js/search-post.js
@@ -104,6 +104,7 @@ function renderResults(results) {
       var a = document.createElement('a');
       a.href = result.url;
       a.innerHTML = result.title;
+      a.classList.add('long-title');
       li.appendChild(a);
       var p = document.createElement('p');
       p.innerHTML = result.excerpt;
@@ -127,6 +128,7 @@ function renderResults(results) {
       var a = document.createElement('a');
       a.href = result.url;
       a.innerHTML = result.title;
+      a.classList.add('long-title');
       li.appendChild(a);
       var p = document.createElement('p');
       p.innerHTML = result.excerpt;
