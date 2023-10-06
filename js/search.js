13c8bafa71a9cf65820aeac4de836f8ebfb88caa
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-10-06 09:43:18 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -33,7 +33,8 @@
           tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
           categories: "{{ post.categories | xml_escape }}",
           date: "{{ post.date | date: "%B %d, %Y" }}",
-          note: "{{- post.content | replace_first: '<p>', '' | replace: '</p>\\s*<p>', '</p><p>' | replace: '"', '\"' | strip_newlines | strip -}}"
+          note: "{{- post.content | replace_first: '<p>', '' | replace: '</p>\\s*<p>', '</p><p>' | replace: '"', '\"' | strip_newlines | strip -}}",
+          content: "{{- post.content | replace: '"', '\"' | strip_newlines | strip -}}"
         }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
@@ -137,7 +138,7 @@
           a.classList.add('long-title');
           li.appendChild(a);
           var p = document.createElement('p');
-          p.innerHTML = result.excerpt;
+          p.innerHTML = result.content;
           li.appendChild(p);
         }
         
@@ -199,7 +200,7 @@
           a.classList.add('long-title');
           li.appendChild(a);
           var p = document.createElement('p');
-          p.innerHTML = result.excerpt;
+          p.innerHTML = result.content;
           li.appendChild(p);
         }
 
