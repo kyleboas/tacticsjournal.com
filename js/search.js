59e9377957ce22eb8a8764af3f0e32f3baa739e6
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-11-13 09:03:40 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -26,16 +26,17 @@
 
   var posts = [
     {% for post in site.posts %}
-    {
+        {
           title: "{{ post.title | xml_escape }}",
           url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
           excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-          tags: "{{ post.tags | xml_escape }}",
+          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
           categories: "{{ post.categories | xml_escape }}",
-          content: "{{ post.content | strip_newlines | strip }}",
-          date: "{{ post.date }}"
-    }
-     {% endfor %}
+          date: "{{ post.date | date: "%B %d, %Y" }}",
+          note: "{{- post.content | replace_first: '<p>', '' | replace: '</p>\\s*<p>', '</p><p>' | replace: '"', '\"' | strip_newlines | strip -}}",
+          content: "{{- post.content | jsonify -}}"
+        }{% unless forloop.last %},{% endunless %}
+    {% endfor %}
   ];
 
   function search(query) {
