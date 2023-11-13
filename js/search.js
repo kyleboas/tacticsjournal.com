d0d020c07d9d9c6f0e5166879406723bf688f093
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-11-13 08:41:19 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,19 +25,18 @@
   }
 
   var posts = [
-    {
     {% for post in site.posts %}
+    {
           title: "{{ post.title | xml_escape }}",
           url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
           excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+          tags: "{{ post.tags | xml_escape }}",
           categories: "{{ post.categories | xml_escape }}",
-          date: "{{ post.date | date: "%B %d, %Y" }}",
-          note: "{{- post.content | replace_first: '<p>', '' | replace: '</p>\\s*<p>', '</p><p>' | replace: '"', '\"' | strip_newlines | strip -}}",
-          content: "{{- post.content | replace: '"', '\"' | strip_newlines | strip -}}"
+          content: "{{ post.content strip_newlines | strip }}",
+          date: "{{ post.date }}"
+    }
      {% unless forloop.last %},{% endunless %} 
      {% endfor %}
-    }
   ];
 
   function search(query) {
