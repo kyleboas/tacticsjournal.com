336ebc7546bac9704b4792831620f97b756ecdc3
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2023-12-31 14:51:31 -0500

    Update question.js

--- js/question.js
+++ js/question.js
@@ -10,3 +10,20 @@ function submitQuestion() {
 
   // You can redirect or perform further actions here
 }
+
+// Modify your existing script.js file to include this function
+function displayQuestions() {
+  // Retrieve submitted question and guest name from localStorage
+  var question = localStorage.getItem('submittedQuestion');
+  var guestName = localStorage.getItem('guestName');
+
+  // Display the question in the questionList
+  var questionList = document.getElementById('questionList');
+  var listItem = document.createElement('li');
+  listItem.textContent = `${guestName}: ${question}`;
+  questionList.appendChild(listItem);
+
+  // Clear localStorage after displaying the question
+  localStorage.removeItem('submittedQuestion');
+  localStorage.removeItem('guestName');
+}
