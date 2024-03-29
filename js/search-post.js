1ab87a267e155c88896b1512e960b9ddcb55073e
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2024-03-29 14:14:42 -0400

    fixed %d %B %Y

--- js/search-post.js
+++ js/search-post.js
@@ -31,7 +31,7 @@
           title: "{{ post.title | xml_escape }}",
           url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
           excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
-          date: "{{ post.date | date: "%B %d, %Y" }}",
+          date: "{{ post.date | date: "%d %B %Y" }}",
           tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
           categories: "{{ post.categories | xml_escape }}"
         }{% unless forloop.last %},{% endunless %}
