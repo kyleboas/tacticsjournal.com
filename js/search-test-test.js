6ba4e10f6f72fa2475ccac412d42de755e6dac12
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2024-06-30 21:18:06 +0000

    search test

---
+++ js/search-test-test.js
@@ -0,0 +1,202 @@
+---
+---
+
+(function () {
+  var searchInputContainer = document.getElementById('search-criteria-container');
+  var postList = document.getElementById('post-list');
+  var noResultsMessage = document.getElementById('no-results-message');
+
+  if (!noResultsMessage) {
+    noResultsMessage = document.createElement('div');
+    noResultsMessage.id = 'no-results-message';
+
+    var imageLink = document.createElement('a');
+    imageLink.href = '{{ site.baseurl }}/';
+    var image = document.createElement('img');
+    image.src = '{{ site.baseurl }}/assets/C689B127-879F-4B0E-9D00-EDC0D8410697.jpeg';
+    image.style.width = '100px';
+    imageLink.appendChild(image);
+    noResultsMessage.appendChild(imageLink);
+
+    noResultsMessage.style.display = 'none'; // Initially hide the message
+    postList.parentNode.insertBefore(noResultsMessage, postList); // Insert the message above the post-list
+  }
+
+  var posts = [
+    {% for post in site.posts %}
+      {% unless post.categories contains 'Notes' %}
+        {
+          title: "{{ post.title | xml_escape }}",
+          url: "{{ site.baseurl }}{{ post.url | xml_escape }}",
+          link: "{{ post.link | xml_escape }}", // Add the link from front matter
+          excerpt: "{{ post.excerpt | strip_html | strip_newlines | escape }}",
+          date: "{{ post.date | date: "%d %B %Y" }}",
+          tags: "{% for tag in post.tags %}{{ tag }}{% unless forloop.last %}, {% endunless %}{% endfor %}",
+          categories: "{{ post.categories | xml_escape }}",
+          content: "{{- post.content | replace: '"', '\"' | strip_newlines | strip -}}"
+        }{% unless forloop.last %},{% endunless %}
+      {% endunless %}
+    {% endfor %}
+  ];
+
+  function search(queries) {
+    var results = [];
+
+    if (queries.length === 0) {
+      return posts; // Return all posts if no query is provided or if it's blank
+    }
+
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+      var match = queries.every(function(query) {
+        return post.title.toLowerCase().includes(query.toLowerCase()) ||
+               post.excerpt.toLowerCase().includes(query.toLowerCase()) ||
+               post.tags.toLowerCase().includes(query.toLowerCase()) || // Add search in tags
+               post.categories.toLowerCase().includes(query.toLowerCase()); // Add search in categories
+      });
+
+      if (match) {
+        var highlightedTitle = highlightMatch(post.title, queries);
+        var highlightedExcerpt = highlightMatch(post.excerpt, queries);
+        results.push({
+          title: highlightedTitle,
+          date: post.date,
+          url: post.url,
+          link: post.link, // Include the link in the results
+          excerpt: highlightedExcerpt,
+          tags: post.tags,
+          categories: post.categories
+        });
+      }
+    }
+
+    return results;
+  }
+
+  function highlightMatch(text, queries) {
+    queries.forEach(function(query) {
+      var regex = new RegExp(query, 'gi');
+      text = text.replace(regex, function (match) {
+        return '<span class="highlight">' + match + '</span>';
+      });
+    });
+    return text;
+  }
+
+  function getCurrentPageUrl() {
+    return window.location.href;
+  }
+
+  function renderResults(results) {
+    postList.innerHTML = '';
+
+    var searchInputs = document.querySelectorAll('#search-criteria-container input');
+    var searchQuery = Array.from(searchInputs).map(input => input.value.trim()).filter(Boolean).join(', ');
+    var countElement = document.getElementById('result-count');
+
+    if (searchQuery === '') {
+      countElement.innerHTML = 'Last 15 posts';
+      noResultsMessage.style.display = 'none';
+
+      // Filter out posts that match the current page's URL
+      var filteredResults = results.filter(function (post) {
+        return post.url.toLowerCase() !== getCurrentPageUrl().toLowerCase();
+      });
+
+      // Show only the first 15 posts
+      var slicedResults = filteredResults.slice(0, 15);
+
+      for (var i = 0; i < slicedResults.length; i++) {
+        var result = slicedResults[i];
+        var li = document.createElement('li');
+        li.classList.add('post-item');
+
+        var a = document.createElement('a');
+        a.href = result.link ? result.link : result.url; // Use link if it exists
+        a.target = '_blank'; // Open link in a new tab
+        a.innerHTML = result.title;
+        a.classList.add('long-title');
+        li.appendChild(a);
+
+        var dateElement = document.createElement('p');
+        dateElement.classList.add('post-date');
+        dateElement.innerHTML = result.date;
+        li.appendChild(dateElement);
+
+        var p = document.createElement('p');
+        if (i === 0) {
+          p.innerHTML = posts[0].content; // Display full content for the first post
+        } else {
+          p.innerHTML = result.excerpt; // Display excerpt for other posts
+        }
+        li.appendChild(p);
+
+        postList.appendChild(li);
+      }
+    } else if (results.length === 0) {
+      countElement.innerHTML = 'No posts found';
+      noResultsMessage.style.display = 'block';
+    } else {
+      var postsShown = results.length;
+      var totalCount = posts.length;
+      countElement.innerHTML = postsShown + ' posts found';
+      noResultsMessage.style.display = 'none';
+
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        var li = document.createElement('li');
+        li.classList.add('post-item');
+
+        var a = document.createElement('a');
+        a.href = result.link ? result.link : result.url; // Use link if it exists
+        a.target = '_blank'; // Open link in a new tab
+        a.innerHTML = result.title;
+        a.classList.add('long-title');
+        li.appendChild(a);
+
+        var dateElement = document.createElement('p');
+        dateElement.classList.add('post-date');
+        dateElement.innerHTML = result.date;
+        li.appendChild(dateElement);
+
+        var p = document.createElement('p');
+        p.innerHTML = result.excerpt;
+        li.appendChild(p);
+
+        postList.appendChild(li);
+      }
+    }
+  }
+
+  // Get the search query from the URL
+  var searchQuery = new URLSearchParams(window.location.search).get('search');
+  if (searchQuery) {
+    var searchInput = document.createElement('input');
+    searchInput.type = 'text';
+    searchInput.placeholder = 'Search...';
+    searchInput.value = searchQuery;
+    searchInputContainer.appendChild(searchInput);
+    searchInput.dispatchEvent(new Event('input'));
+  }
+
+  searchInputContainer.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      var newSearchInput = document.createElement('input');
+      newSearchInput.type = 'text';
+      newSearchInput.placeholder = 'Search...';
+      searchInputContainer.appendChild(newSearchInput);
+      newSearchInput.focus();
+    }
+  });
+
+  searchInputContainer.addEventListener('input', function () {
+    var searchInputs = document.querySelectorAll('#search-criteria-container input');
+    var queries = Array.from(searchInputs).map(input => input.value.trim()).filter(Boolean);
+    var results = search(queries);
+    renderResults(results);
+  });
+
+  // Initial render of the first 15 posts
+  renderResults(posts.slice(0, 15));
+})();
--- preview.md
+++ preview.md
@@ -3,24 +3,17 @@ layout: default
 permalink: /preview/
 ---
 
-<!-- index.html -->
 <div class="searchInput">
-  <input type="text" id="search-input" placeholder="Search...">
-  <input type="text" id="search-tags-1" placeholder="Search by team...">
-  <input type="text" id="search-tags-2" placeholder="Search by team...">
-  <input type="text" id="search-categories" placeholder="Search by competition...">
-  <p id="p-result-count" style="margin-top: 0px;"><span id="result-count">Last 15 posts</span></p>
+  <div id="search-criteria-container">
+    <input type="text" id="search-input" placeholder="Search...">
+  </div>
+  <p id="p-result-count" style="margin-top: 0px;"><span id="result-count"></span></p>
+  <div class="resultBox">
+    <!-- here list are inserted from javascript -->
+  </div>
 </div>
 
-<ul id="post-list">
-  {% for post in site.posts limit:15 %}
-    <li class="post-item initial-post">
-      <a href="{{ post.link | default: post.url }}" target="_blank" class="long-title">{{ post.title }}</a>
-      <p class="post-date">{{ post.date | date: "%d %B %Y" }}</p>
-      <p>{{ post.excerpt | strip_html | strip_newlines | escape }}</p>
-    </li>
-  {% endfor %}
-</ul>
+<ul id="post-list"></ul>
 
 <hr>
 
@@ -31,24 +24,13 @@ permalink: /preview/
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var searchQuery = urlParams.get("search");
-    var searchTags1 = urlParams.get("tags1");
-    var searchTags2 = urlParams.get("tags2");
-    var searchCategories = urlParams.get("categories");
 
-    if (searchQuery || searchTags1 || searchTags2 || searchCategories) {
+    if (searchQuery) {
       var searchInput = document.getElementById("search-input");
-      var searchTagsInput1 = document.getElementById("search-tags-1");
-      var searchTagsInput2 = document.getElementById("search-tags-2");
-      var searchCategoriesInput = document.getElementById("search-categories");
-
-      if (searchQuery) searchInput.value = searchQuery;
-      if (searchTags1) searchTagsInput1.value = searchTags1;
-      if (searchTags2) searchTagsInput2.value = searchTags2;
-      if (searchCategories) searchCategoriesInput.value = searchCategories;
-
+      searchInput.value = searchQuery;
       searchInput.dispatchEvent(new Event("input"));
     }
   });
 </script>
-<script src="/js/search-test.js"></script>
-<script src="/js/suggest-test.js"></script>
+<script src="/js/search-test-test.js"></script>
+<script src="/js/suggest.js"></script>
